7d29885ee3b8097a31220e5aac704f022b8b0de6
kiran <kkbarik2031@gmail.com>
2020-11-20 08:05:24 +0530

    creating git_demo project with adding some classes

---
+++ src/main/java/com/git/basic/example/AccountUtility.java
@@ -0,0 +1,26 @@
+package com.git.basic.example;
+
+public class AccountUtility {
+
+	
+	public void checkInput(String name) {
+		
+		if(name.equals("NSE"))
+			System.out.println("NSE USER");
+		else
+			System.out.println("CENTRAL USER");
+		
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+		System.out.println("Hi Good Morning...");
+		
+		
+	}
+	
+	
+}
